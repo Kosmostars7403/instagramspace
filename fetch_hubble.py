e3b9b1420d55943159fd72b3e067688267a89fff
iCherniakov <3699818@mail.ru>
2020-05-21 10:53:51 +0300

    Optimized the code

--- fetch_hubble.py
+++ fetch_hubble.py
@@ -1,17 +1,18 @@
 import requests
 from download_func import download_image
 import argparse
+from os import path
 
-def extension(url):
-    a = url.split('.')
-    return a[-1]
+def get_extension(url):
+    split_url = path.splitext(url)
+    return split_url[1]
 
 
-def hubble_image(id):
+def fetch_hubble_image(id):
     url = ('http://hubblesite.org/api/v3/image/{}').format(id)
     response = requests.get(url).json()
     image_link = 'http:' + response['image_files'][-1]['file_url']
-    filename = ('./images/{}.{}').format(str(id), extension(image_link))
+    filename = ('./images/{}{}').format(str(id), get_extension(image_link))
     download_image(image_link, filename)
 
 
@@ -19,8 +20,7 @@ def download_hubble_collection(collection_name):
     url = ('http://hubblesite.org/api/v3/images/{}').format(collection_name)
     response = requests.get(url).json()
     for pic in response:
-        print('Downloading picture:', pic['name'])
-        hubble_image(pic['id'])
+        fetch_hubble_image(pic['id'])
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Скачиваем коллекции фотографий с hubblesite.org')
--- instagram_upload.py
+++ instagram_upload.py
@@ -4,31 +4,35 @@ import instabot
 from PIL import Image
 from dotenv import load_dotenv
 
-def instagram_thumbnail(filename):
-    name = (filename.replace('/', '.')).split('.')
+def get_name(path):
+    split_path = (path.replace('/', '.')).split('.')
+    return split_path[-2]
+
+def create_instagram_thumbnail(filename):
+    name = get_name(filename)
     image = Image.open(filename)
     image.thumbnail((1080, 1080))
     Path("./insta_images").mkdir(parents=True, exist_ok=True)
     rgb = image.convert('RGB')
-    rgb.save(("./insta_images/{}.jpg").format(name[-2]), format="JPEG")
+    rgb.save(("./insta_images/{}.jpg").format(name), format="JPEG")
 
 
 def upload_images(filename):
     bot = instabot.Bot()
     bot.login(username=LOGIN, password=PASSWORD)
-    name = (filename.replace('/', '.')).split('.')
-    bot.upload_photo(('./insta_images/{}.jpg').format(name[-2]), caption="Nice pic!")
+    name = get_name(filename)
+    bot.upload_photo(('./insta_images/{}.jpg').format(name), caption="Nice pic!")
 
 
 if __name__ == '__main__':
     load_dotenv()
-    LOGIN = getenv('LOGIN')
-    PASSWORD = getenv('PASSWORD')
+    login = getenv('LOGIN')
+    password = getenv('PASSWORD')
 
     images = listdir('images')
 
     for image in images:
-        instagram_thumbnail(('./images/{}').format(image))
+        create_instagram_thumbnail(('./images/{}').format(image))
         upload_images(image)
 
 
